62c511e13ae55b521ad401aa61d9684d517e4c20
LeManh <manhzxc3@gmail.com>
2023-02-02 16:54:13 +0700

    destructuring day 1

--- FileCode/example/Section 9_Data Structures, Modern Operators and Strings/Destructuring_Arrays.js
+++ FileCode/example/Section 9_Data Structures, Modern Operators and Strings/Destructuring_Arrays.js
@@ -1,15 +1,38 @@
 'use trict';
-const restaurant = {
-    name: 'VietNam Food',
-    location: 'Chu Phan, Me Linh, Ha Noi',
-    categories: ['Banh ', 'Pho', 'Com'],
-    starterMenu: ['Salad', 'Khoai Tay Chien', 'Nuoc Ngot'],
-    mainMenu: ['Com Tam', 'Pho Bo', 'Banh Xeo'],
-}
+const nhahang = {
+    ten: 'VietNam Food',
+    vitri: 'Chu Phan, Me Linh, Ha Noi',
+    theloai: ['Mon Viet ', 'Tiem Banh Pizza', 'An Chay', 'Huu Co'],
+    monkhaivi: ['Salad', 'Khoai Tay Chien', 'Nuoc Ngot'],
+    monchinh: ['Com Tam', 'Pho Bo', 'Banh Xeo'],
+    goi: function (khaiviIndex, chinhIndex) {
+        return [this.monkhaivi[khaiviIndex], this.monchinh[chinhIndex]];
+    },
+};
 const arr = [2, 3, 4];
 const a = arr[0];
 const b = arr[1];
 const c = arr[2];
 
 const [x, y, z] = arr;
-console.log(x, y, z);
+console.log(arr);
+
+let [main, , secondary] = nhahang.theloai;
+// - dau " , " se bo qua phan tu
+// => Mon Viet, An Chay
+console.log(main, secondary);
+
+// ==== Switching variables ====
+// const temp = main; // - bien tam
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+// => An Chay, Mon Viet
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+console.log(nhahang.goi(2,0));
+// => Nuoc Ngot, Com Tam
+const [starter, mainCouser] = nhahang.goi(2,0);
+console.log(starter, mainCouser);
+
