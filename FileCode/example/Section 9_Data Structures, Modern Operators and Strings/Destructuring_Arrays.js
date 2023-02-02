ec0b3458eb964ddf70b2dd43e85829990b419cfe
LeManh <manhzxc3@gmail.com>
2023-02-02 18:08:42 +0700

    Bai 2

--- FileCode/example/Section 9_Data Structures, Modern Operators and Strings/Destructuring_Arrays.js
+++ FileCode/example/Section 9_Data Structures, Modern Operators and Strings/Destructuring_Arrays.js
@@ -1,12 +1,12 @@
 'use trict';
-const nhahang = {
-    ten: 'VietNam Food',
-    vitri: 'Chu Phan, Me Linh, Ha Noi',
-    theloai: ['Mon Viet ', 'Tiem Banh Pizza', 'An Chay', 'Huu Co'],
-    monkhaivi: ['Salad', 'Khoai Tay Chien', 'Nuoc Ngot'],
-    monchinh: ['Com Tam', 'Pho Bo', 'Banh Xeo'],
-    goi: function (khaiviIndex, chinhIndex) {
-        return [this.monkhaivi[khaiviIndex], this.monchinh[chinhIndex]];
+const restaurant = {
+    name: 'VietNam Food',
+    location: 'Chu Phan, Me Linh, Ha Noi',
+    categories: ['Mon Viet ', 'Tiem Banh Pizza', 'An Chay', 'Huu Co'],
+    starterMenu: ['Salad', 'Khoai Tay Chien', 'Nuoc Ngot'],
+    mainMenu: ['Com Tam', 'Pho Bo', 'Banh Xeo'],
+    oder: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
 };
 const arr = [2, 3, 4];
@@ -17,7 +17,7 @@ const c = arr[2];
 const [x, y, z] = arr;
 console.log(arr);
 
-let [main, , secondary] = nhahang.theloai;
+let [main, , secondary] = restaurant.categories;
 // - dau " , " se bo qua phan tu
 // => Mon Viet, An Chay
 console.log(main, secondary);
@@ -31,8 +31,17 @@ console.log(main, secondary);
 [main, secondary] = [secondary, main];
 console.log(main, secondary);
 
-console.log(nhahang.goi(2,0));
+console.log(restaurant.oder(2,0));
 // => Nuoc Ngot, Com Tam
-const [starter, mainCouser] = nhahang.goi(2,0);
+const [starter, mainCouser] = restaurant.oder(2,0);
 console.log(starter, mainCouser);
 
+const nested = [2,4,[5,6]];
+
+const [i, , [j, k]] = nested;
+console.log(i , j ,k);
+
+// default values
+
+const [p = 1,q = 1,r = 1] = [8 , 9];
+console.log(p,q,r);
---
+++ FileCode/example/Section 9_Data Structures, Modern Operators and Strings/Destructuring_Objects.js
@@ -0,0 +1,67 @@
+'use trict';
+const restaurant = {
+    name: 'VietNam Food',
+    location: 'Chu Phan, Me Linh, Ha Noi',
+    categories: ['Mon Viet ', 'Tiem Banh Pizza', 'An Chay', 'Huu Co'],
+    starterMenu: ['Salad', 'Khoai Tay Chien', 'Nuoc Ngot'],
+    mainMenu: ['Com Tam', 'Pho Bo', 'Banh Xeo'],
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0,
+            close: 24,
+        },
+    },
+    oder: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    oderDelivery: function ({starterIndex, mainIndex, time, address}) {
+        console.log(`oder received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`);
+    },
+};
+restaurant.oderDelivery({
+    time: '22:30',
+    address: 'HN',
+    mainIndex: 2,
+    starterIndex: 2,
+});
+
+// const {name, openingHours, categories} = restaurant;
+
+// console.log(name, openingHours, categories);
+
+// có thể đặt thành tên biến khác 
+
+// const {
+//     name: restaurantName,
+//     openingHours: hours,
+//     categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
+
+// định nghĩa lại trong trường hợp tên giá trị đó không tồn tại, khi API gửi về
+// menu ở đây chưa có 
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// // Mutating Variables - Hoán đổi
+
+// let a = 111;
+// let b = 999;
+// const obj = { a: 1, b: 2, c: 3 };
+// ({a,b} = obj);
+// console.log(a, b);
+
+// // Nested obj
+
+// const {
+//     fri: {open, close},
+// } = hours; // được đổi ở dòng 35
+// console.log(open,close);
